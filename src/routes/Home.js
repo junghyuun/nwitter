8733b99c91a47cf93f9e7f4783989532afc91839
junghyuni <anjjang23@gmail.com>
2021-04-19 18:20:27 +0900

    get the nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { dbService } from "fbase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -12,7 +28,7 @@ const Home = () => {
     });
     setNweet("");
   };
-  
+
   const onChange = (e) => {
     const {
       target: { value },
@@ -20,6 +36,8 @@ const Home = () => {
     setNweet(value);
   };
 
+  console.log(nweets);
+  
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -32,6 +50,14 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
