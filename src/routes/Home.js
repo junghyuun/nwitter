547b10ed20b28d99d16badd591dda12bf24aad32
junghyuni <anjjang23@gmail.com>
2021-04-19 18:07:23 +0900

    nweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
+import { dbService } from "fbase";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
-  const onSubmit = (e) => {
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
   };
+  
   const onChange = (e) => {
     const {
       target: { value },
