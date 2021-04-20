d09f4fe7ee54247e8de1c4e1e91ab8dd77fca365
junghyuni <anjjang23@gmail.com>
2021-04-20 17:37:08 +0900

    profile edit

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,28 @@
-import React from "react";
-import { authService } from "fbase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fbase";
 import { useHistory } from "react-router-dom";
 
 // eslint-disable-next-line
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+    // eslint-disable-next-line
+  }, []);
 
   return (
     <>
